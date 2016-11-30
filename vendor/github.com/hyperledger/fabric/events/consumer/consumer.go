70c34a6775e4d11a9bb6ccf125ccf4f63b281d13
xiebo <xiebo@qkldx.net>
2016-11-30 20:16:11 +0800

    修改：日志输出为调试信息

--- vendor/github.com/hyperledger/fabric/events/consumer/consumer.go
+++ vendor/github.com/hyperledger/fabric/events/consumer/consumer.go
@@ -1,4 +1,20 @@
-package blockchain
+/*
+Copyright IBM Corp. 2016 All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+		 http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package consumer
 
 import (
 	"fmt"
@@ -6,40 +22,52 @@ import (
 	"sync"
 	"time"
 
-	"github.com/hyperledger/fabric/events/consumer"
-	fabricpb "github.com/hyperledger/fabric/protos"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+
+	"github.com/hyperledger/fabric/core/comm"
+	ehpb "github.com/hyperledger/fabric/protos"
 )
 
-type (
-	eventsClient struct {
-		sync.RWMutex
-		conn       *grpc.ClientConn
-		regTimeout time.Duration
-		stream     fabricpb.Events_ChatClient
-		adapter    consumer.EventAdapter
-	}
-)
-
-func newEventsClient(conn *grpc.ClientConn, adapter consumer.EventAdapter, regTimeout time.Duration) *eventsClient {
-	ecli := eventsClient{}
-	ecli.RWMutex = sync.RWMutex{}
-	ecli.conn = conn
-	ecli.regTimeout = regTimeout
-	ecli.adapter = adapter
-	return &ecli
+//EventsClient holds the stream and adapter for consumer to work with
+type EventsClient struct {
+	sync.RWMutex
+	peerAddress string
+	regTimeout  time.Duration
+	stream      ehpb.Events_ChatClient
+	adapter     EventAdapter
 }
 
-func (ec *eventsClient) send(emsg *fabricpb.Event) error {
+//NewEventsClient Returns a new grpc.ClientConn to the configured local PEER.
+func NewEventsClient(peerAddress string, regTimeout time.Duration, adapter EventAdapter) (*EventsClient, error) {
+	var err error
+	if regTimeout < 100*time.Millisecond {
+		regTimeout = 100 * time.Millisecond
+		err = fmt.Errorf("regTimeout >= 0, setting to 100 msec")
+	} else if regTimeout > 60*time.Second {
+		regTimeout = 60 * time.Second
+		err = fmt.Errorf("regTimeout > 60, setting to 60 sec")
+	}
+	return &EventsClient{sync.RWMutex{}, peerAddress, regTimeout, nil, adapter}, err
+}
+
+//newEventsClientConnectionWithAddress Returns a new grpc.ClientConn to the configured local PEER.
+func newEventsClientConnectionWithAddress(peerAddress string) (*grpc.ClientConn, error) {
+	if comm.TLSEnabled() {
+		return comm.NewClientConnectionWithAddress(peerAddress, true, true, comm.InitTLSForPeer())
+	}
+	return comm.NewClientConnectionWithAddress(peerAddress, true, false, nil)
+}
+
+func (ec *EventsClient) send(emsg *ehpb.Event) error {
 	ec.Lock()
 	defer ec.Unlock()
 	return ec.stream.Send(emsg)
 }
 
 // RegisterAsync - registers interest in a event and doesn't wait for a response
-func (ec *eventsClient) RegisterAsync(ies []*fabricpb.Interest) error {
-	emsg := &fabricpb.Event{Event: &fabricpb.Event_Register{Register: &fabricpb.Register{Events: ies}}}
+func (ec *EventsClient) RegisterAsync(ies []*ehpb.Interest) error {
+	emsg := &ehpb.Event{Event: &ehpb.Event_Register{Register: &ehpb.Register{Events: ies}}}
 	var err error
 	if err = ec.send(emsg); err != nil {
 		fmt.Printf("error on Register send %s\n", err)
@@ -48,11 +76,12 @@ func (ec *eventsClient) RegisterAsync(ies []*fabricpb.Interest) error {
 }
 
 // register - registers interest in a event
-func (ec *eventsClient) register(ies []*fabricpb.Interest) error {
+func (ec *EventsClient) register(ies []*ehpb.Interest) error {
 	var err error
 	if err = ec.RegisterAsync(ies); err != nil {
 		return err
 	}
+
 	regChan := make(chan struct{})
 	go func() {
 		defer close(regChan)
@@ -62,7 +91,7 @@ func (ec *eventsClient) register(ies []*fabricpb.Interest) error {
 			return
 		}
 		switch in.Event.(type) {
-		case *fabricpb.Event_Register:
+		case *ehpb.Event_Register:
 		case nil:
 			err = fmt.Errorf("invalid nil object for register")
 		default:
@@ -78,21 +107,23 @@ func (ec *eventsClient) register(ies []*fabricpb.Interest) error {
 }
 
 // UnregisterAsync - Unregisters interest in a event and doesn't wait for a response
-func (ec *eventsClient) UnregisterAsync(ies []*fabricpb.Interest) error {
-	emsg := &fabricpb.Event{Event: &fabricpb.Event_Unregister{Unregister: &fabricpb.Unregister{Events: ies}}}
+func (ec *EventsClient) UnregisterAsync(ies []*ehpb.Interest) error {
+	emsg := &ehpb.Event{Event: &ehpb.Event_Unregister{Unregister: &ehpb.Unregister{Events: ies}}}
 	var err error
 	if err = ec.send(emsg); err != nil {
 		err = fmt.Errorf("error on unregister send %s\n", err)
 	}
+
 	return err
 }
 
 // unregister - unregisters interest in a event
-func (ec *eventsClient) unregister(ies []*fabricpb.Interest) error {
+func (ec *EventsClient) unregister(ies []*ehpb.Interest) error {
 	var err error
 	if err = ec.UnregisterAsync(ies); err != nil {
 		return err
 	}
+
 	regChan := make(chan struct{})
 	go func() {
 		defer close(regChan)
@@ -102,7 +133,7 @@ func (ec *eventsClient) unregister(ies []*fabricpb.Interest) error {
 			return
 		}
 		switch in.Event.(type) {
-		case *fabricpb.Event_Unregister:
+		case *ehpb.Event_Unregister:
 		case nil:
 			err = fmt.Errorf("invalid nil object for unregister")
 		default:
@@ -118,7 +149,7 @@ func (ec *eventsClient) unregister(ies []*fabricpb.Interest) error {
 }
 
 // Recv recieves next event - use when client has not called Start
-func (ec *eventsClient) Recv() (*fabricpb.Event, error) {
+func (ec *EventsClient) Recv() (*ehpb.Event, error) {
 	in, err := ec.stream.Recv()
 	if err == io.EOF {
 		// read done.
@@ -135,7 +166,7 @@ func (ec *eventsClient) Recv() (*fabricpb.Event, error) {
 	}
 	return in, nil
 }
-func (ec *eventsClient) processEvents() error {
+func (ec *EventsClient) processEvents() error {
 	defer ec.stream.CloseSend()
 	for {
 		in, err := ec.stream.Recv()
@@ -162,36 +193,41 @@ func (ec *eventsClient) processEvents() error {
 }
 
 //Start establishes connection with Event hub and registers interested events with it
-func (ec *eventsClient) Start() error {
+func (ec *EventsClient) Start() error {
+	conn, err := newEventsClientConnectionWithAddress(ec.peerAddress)
+	if err != nil {
+		return fmt.Errorf("Could not create client conn to %s", ec.peerAddress)
+	}
+
 	ies, err := ec.adapter.GetInterestedEvents()
 	if err != nil {
 		return fmt.Errorf("error getting interested events:%s", err)
 	}
+
 	if len(ies) == 0 {
 		return fmt.Errorf("must supply interested events")
 	}
-	serverClient := fabricpb.NewEventsClient(ec.conn)
+
+	serverClient := ehpb.NewEventsClient(conn)
 	ec.stream, err = serverClient.Chat(context.Background())
 	if err != nil {
-		return err
+		return fmt.Errorf("Could not create client conn to %s", ec.peerAddress)
 	}
+
 	if err = ec.register(ies); err != nil {
 		return err
 	}
+
 	go ec.processEvents()
+
 	return nil
 }
 
 //Stop terminates connection with event hub
-func (ec *eventsClient) Stop() error {
+func (ec *EventsClient) Stop() error {
 	if ec.stream == nil {
 		// in case the steam/chat server has not been established earlier, we assume that it's closed, successfully
 		return nil
 	}
-	err := ec.stream.CloseSend()
-	if err != nil {
-		ec.conn.Close()
-		return err
-	}
-	return ec.conn.Close()
+	return ec.stream.CloseSend()
 }
